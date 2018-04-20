0a849a48570cf2fac4eb43cc67c6eb764a24960c
Yang Zhou <yzhou3991@gmail.com>
2018-04-20 01:36:32 -0400

    build initial towers

---
+++ game.js
@@ -0,0 +1,27 @@
+class Game {
+  constructor(numberOfTowers){
+    this.towers = this.buildTowers(numberOfTowers);
+  }
+
+  buildTowers(num){
+    let result = [];
+    let firstTower = [];
+    let i = num;
+    while(i>0){
+      firstTower.push(i);
+      i--;
+    }
+    for (let j = 0; j < num; j++){
+      if (j !== 0) {
+        result.push([]);
+      }
+      else {
+        result.push(firstTower);
+      }
+    }
+    return result;
+  }
+}
+
+let g = new Game(4);
+console.log(g.towers)
