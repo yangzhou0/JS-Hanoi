2e19394bf8ad19f3a3188541d5900543dc2153e4
Yang Zhou <yzhou3991@gmail.com>
2018-04-20 23:44:35 -0400

    implement move method; debug print method

--- game.js
+++ game.js
@@ -20,13 +20,13 @@ class Game {
   }
 
   print(){
-    let towers = this.towers;
+    let towers = JSON.parse(JSON.stringify(this.towers)); //deep copy tower;
     let num = towers.length;
     let base = '\n0  1  2';
     while(towers.some((x)=>x.length>0)) {
       let line = ''
       towers.forEach((tower)=>{
-        let item = tower[0] === undefined ? ' ' : tower.shift()
+        let item = tower[0] === undefined ? ' ' : tower.shift() // this part may cause problem if i didnt deep clone at first
         line += `${item}  `;
       })
       base = line + '\n' + base;
@@ -37,9 +37,22 @@ class Game {
   isValidMove(startTowerIdx, endTowerIdx){
     let fromTower = this.towers[startTowerIdx];
     let toTower = this.towers[endTowerIdx];
-    if(_.isEmpty(fromTower)) {return false;}
-    else if (_.isEmpty(toTower)) {return true;}
+    if(_.isEmpty(fromTower)) {console.log('invalid move'); return false;}
+    else if (_.isEmpty(toTower)) {console.log('valid move'); return true;}
     else{return _.last(fromTower) < _.last(toTower);}
+  }
+
+  move(startTowerIdx, endTowerIdx){
+
+    let fromTower = this.towers[startTowerIdx];
+    let toTower = this.towers[endTowerIdx];
+    if(this.isValidMove(startTowerIdx,endTowerIdx)) {
+      toTower.push(fromTower.pop());
+      this.print();
+      return true;
+    }
+    this.print();
+    return false;
   }
 
   static buildTowers(num){
@@ -65,6 +78,9 @@ class Game {
 let g = new Game(3);
 // g.promptMove(reader,(from,to)=>console.log(from + ' ' + to)); // test for promptMove
 //tests for isValidMove
-console.log(g.isValidMove(1,2));
-console.log(g.isValidMove(2,1));
-console.log(g.isValidMove(0,1));
+// console.log(g.isValidMove(1,2));
+// console.log(g.isValidMove(2,1));
+// console.log(g.isValidMove(0,1));
+// g.move(1,2);
+g.move(0,1);
+g.move(1,2);
